2d946ae74338cd38775aaab6bd04d6d9e301ad2b
Landon Fuller <landonf@plausible.coop>
2016-09-15 18:31:39 -0600

    Update example run script to include the siba sources, and the GPL PHY sources.

--- run.sh
+++ run.sh
@@ -1,16 +1,34 @@
 #!/bin/sh
 
-BWN_SRCS="if_bwn.c if_bwn_phy_common.c if_bwn_phy_g.c \
-    if_bwn_phy_lp.c if_bwn_phy_n.c if_bwn_util.c"
-
 ARG0=$0
 BASEDIR=`pwd`/`dirname $0`
 
-SRCROOT=$1
-OBJDIR=$2
+SRCROOT=$(realpath $1)
+OBJDIR=$(realpath $2)
 KERNCONF=${3:-GENERIC}
 
-BWN_DEPS="${BASEDIR}/obj/srcport"
+BWN_SRC="${SRCROOT}/dev/bwn"
+BWN_GNU_SRC="${SRCROOT}/gnu/dev/bwn/phy_n"
+BWN_SRCS="\
+	${BWN_SRC}/if_bwn.c \
+	${BWN_SRC}/if_bwn_phy_common.c \
+	${BWN_SRC}/if_bwn_phy_g.c \
+	${BWN_SRC}/if_bwn_phy_lp.c \
+	${BWN_SRC}/if_bwn_phy_n.c \
+	${BWN_SRC}/if_bwn_util.c
+	${BWN_GNU_SRC}/if_bwn_phy_n_core.c
+	${BWN_GNU_SRC}/if_bwn_phy_n_ppr.c
+	${BWN_GNU_SRC}/if_bwn_phy_n_tables.c
+	${BWN_GNU_SRC}/if_bwn_radio_2055.c
+	${BWN_GNU_SRC}/if_bwn_radio_2056.c
+	${BWN_GNU_SRC}/if_bwn_radio_2057.c"
+
+SIBA_SRC="${SRCROOT}/dev/siba"
+SIBA_SRCS="\
+	${SIBA_SRC}/siba_core.c \
+	${SIBA_SRC}/siba_bwn.c"
+
+SRCPORT="${BASEDIR}/obj/srcport"
 
 usage() {
 	echo "Usage: ${ARG0} <freebsd src> <obj dir> [kernconf]"
@@ -33,26 +51,21 @@ if [ ! -d "${OBJDIR}" ]; then
 fi
 
 # Resolve absolute paths
-SRCROOT=$(cd "$SRCROOT" && realpath `pwd`)
-OBJDIR=$(cd "$OBJDIR" && realpath `pwd`)
-KERNOBJS="${OBJDIR}/${SRCROOT}/sys/${KERNCONF}"
+KERNOBJS="${OBJDIR}/${SRCROOT}/${KERNCONF}"
 
 # Build (or rebuild) the tool
-mkdir -p "${BASEDIR}/obj" || exit 1
-make -C "${BASEDIR}" || exit 1
+mkdir -p "${BASEDIR}/obj" 1>&2 || exit 1
+make -C "${BASEDIR}" 1>&2 || exit 1
 
-if [ ! -x "${BWN_DEPS}" ]; then
-	echo "${BWN_DEPS} not found"
+if [ ! -x "${SRCPORT}" ]; then
+	echo "${SRCPORT} not found"
 	exit 1
 fi
 
 # Source cflags
-CFLAGS="-I${KERNOBJS} -I${SRCROOT}/sys -I${SRCROOT}/sys/sys -fno-builtin -nostdinc -D_KERNEL -DINVARIANTS -DINVARIANT_SUPPORT -Wno-pointer-sign"
+CFLAGS="-I${KERNOBJS} -I${SRCROOT} -I${SRCROOT}/sys -fno-builtin -nostdinc -D_KERNEL -DINVARIANTS -DINVARIANT_SUPPORT -Wno-pointer-sign -Wno-shift-count-overflow"
 
-cd "${SRCROOT}/sys/dev/bwn" || exit 1
+${SRCPORT} -host-path="${SRCROOT}/dev/siba" -src-path="${OBJDIR}" -src-path="${SRCROOT}" --format=bwn_stubs ${BWN_SRCS} ${SIBA_SRCS} -- ${CFLAGS} || exit 1
 
-${BWN_DEPS} -host-path="${SRCROOT}/sys/dev/siba" -src-path="${OBJDIR}" -src-path="${SRCROOT}" ${BWN_SRCS} -- ${CFLAGS}
-RET=$?
-echo "Done"
-
-exit $RET
+echo "Analysis Complete" 1>&2
+exit 0
